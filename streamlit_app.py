34cb03812ba3d8fb53891c8e23342d00cc6b3700
Souvendra <114628520+souvendra@users.noreply.github.com>
2022-10-08 01:30:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,8 @@ def get_fruit_load_list():
 
   def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-    return "Thanks for adding + new_fruit"
+      my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+      return "Thanks for adding " + new_fruit
 
 # New Section to Display fruitvice api response
 streamlit.header("Fruityvice Fruit Advice!")
